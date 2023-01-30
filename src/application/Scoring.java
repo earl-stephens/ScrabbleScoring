93bb31325bd634fc7545b934dbc43eb69c399f75
earl-stephens <stephensearl@hotmail.com>
2023-01-30 13:38:53 -0700

    Test and code for scoring of a word

--- src/application/Scoring.java
+++ src/application/Scoring.java
@@ -1,5 +1,49 @@
 package application;
 
-public class Scoring {
+import java.util.HashMap;
 
+public class Scoring {
+	private HashMap<String, Integer> letterScores = new HashMap<String, Integer>();
+	
+	public Scoring() {
+		letterScores.put("A", 1);
+		letterScores.put("B", 3);
+		letterScores.put("C", 3);
+		letterScores.put("D", 2);
+		letterScores.put("E", 1);
+		letterScores.put("F", 4);
+		letterScores.put("G", 2);
+		letterScores.put("H", 4);
+		letterScores.put("I", 1);
+		letterScores.put("J", 8);
+		letterScores.put("K", 5);
+		letterScores.put("L", 1);
+		letterScores.put("M", 3);
+		letterScores.put("N", 1);
+		letterScores.put("O", 1);
+		letterScores.put("P", 3);
+		letterScores.put("Q", 10);
+		letterScores.put("R", 1);
+		letterScores.put("S", 1);
+		letterScores.put("T", 1);
+		letterScores.put("U", 1);
+		letterScores.put("V", 4);
+		letterScores.put("W", 4);
+		letterScores.put("X", 8);
+		letterScores.put("Y", 4);
+		letterScores.put("Z", 10);
+		}
+	
+	public int scores(String word) {
+		int value = 0;
+		char[] letters;
+		letters = word.toCharArray();
+		
+		for(char letter : letters) {
+			String letterKey = String.valueOf(letter).toUpperCase();
+			value += letterScores.get(letterKey);
+		}
+		
+		return value;
+	}
 }
--- src/test/ScoringTest.java
+++ src/test/ScoringTest.java
@@ -7,12 +7,19 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class ScoringTest {
-
+	Scoring scoring = new Scoring();
 	@Test
 	void testForScoringObject() {
-		Scoring scoring = new Scoring();
+		//Scoring scoring = new Scoring();
 		
 		Assert.assertNotNull(scoring);
+	}
+	
+	@Test
+	void testForScoresFromAnArray() {
+		String word = "hello";
+		
+		Assert.assertEquals(8, scoring.scores(word));
 	}
 
 }
